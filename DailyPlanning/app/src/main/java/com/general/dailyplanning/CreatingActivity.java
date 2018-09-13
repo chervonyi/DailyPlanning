0e914904b193ae350bb8fcec557da52561123555
chervonyi <yuri.ternopil@gmail.com>
2018-09-13 11:11:18 -0500

    Made "Creating a new task" panel
    Panel have been finished but not still connected to other screens.

---
+++ DailyPlanning/app/src/main/java/com/general/dailyplanning/CreatingActivity.java
@@ -0,0 +1,13 @@
+package com.general.dailyplanning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creating);
+    }
+}
